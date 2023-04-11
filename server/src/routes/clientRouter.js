5d69046f982eab27d39fc871446c2a228c45cc6b
Nick Alberts <nwalberts5@gmail.com>
2023-04-11 11:39:13 -0400

    initial commit

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -3,7 +3,9 @@ import getClientIndexPath from "../config/getClientIndexPath.js"
 
 const router = new express.Router()
 
+
 const clientRoutes = ["/client", "/name", "/"]
+// for any of the above paths, render the html file that has our div with an id of app! /client/public/index.html
 router.get(clientRoutes, (req, res) => {
   res.sendFile(getClientIndexPath())
 })
--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -5,9 +5,11 @@ import questionsRouter from "./api/v1/questionsRouter.js"
 
 const rootRouter = new express.Router()
 
+// each of these api routers are for our fetch requests
 rootRouter.use("/api/v1/launchers", launchersRouter)
 rootRouter.use("/api/v1/questions", questionsRouter)
 
+// this router will handle non-fetch requests, and will simply render the React app if requested
 rootRouter.use("/", clientRouter)
 
 export default rootRouter
